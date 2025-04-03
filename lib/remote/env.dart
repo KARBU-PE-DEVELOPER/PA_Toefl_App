843632bef87a214cf4cb9e01cbbd0e88b3a6937b
Dony-Ahmad-Hisyam <donyahmad4321@gmail.com>
2025-04-03 18:03:06 +0700

    jawaban sudah berubah warna dan add history page

--- lib/remote/env.dart
+++ lib/remote/env.dart
@@ -27,20 +27,30 @@
 //   static const String gameUrl = 'http://10.0.2.38:9000/api/minigames';
 // }
 
+// class Env {
+//   const Env._();
+//   static const String apiUrl =
+//       'https://upright-elephant-concise.ngrok-free.app/api';
+//   static const String storageUrl =
+//       'https://upright-elephant-concise.ngrok-free.app';
+//   static const String mainUrl =
+//       'https://upright-elephant-concise.ngrok-free.app/api';
+//   static const String secondaryStorageUrl =
+//       'https://upright-elephant-concise.ngrok-free.app/api';
+//   static const String userUrl =
+//       'https://upright-elephant-concise.ngrok-free.app/api';
+//   static const String simulationUrl =
+//       'https://upright-elephant-concise.ngrok-free.app/api';
+//   static const String gameUrl =
+//       'https://upright-elephant-concise.ngrok-free.app/api/minigames';
+// }
 class Env {
   const Env._();
-  static const String apiUrl =
-      'https://upright-elephant-concise.ngrok-free.app/api';
-  static const String storageUrl =
-      'https://upright-elephant-concise.ngrok-free.app';
-  static const String mainUrl =
-      'https://upright-elephant-concise.ngrok-free.app/api';
-  static const String secondaryStorageUrl =
-      'https://upright-elephant-concise.ngrok-free.app/api';
-  static const String userUrl =
-      'https://upright-elephant-concise.ngrok-free.app/api';
-  static const String simulationUrl =
-      'https://upright-elephant-concise.ngrok-free.app/api';
-  static const String gameUrl =
-      'https://upright-elephant-concise.ngrok-free.app/api/minigames';
+  static const String apiUrl = 'http://103.106.72.182:8040/api';
+  static const String storageUrl = 'http://103.106.72.182:8040';
+  static const String mainUrl = 'http://103.106.72.182:8040/api';
+  static const String secondaryStorageUrl = 'http://103.106.72.182:8040/api';
+  static const String userUrl = 'http://103.106.72.182:8040/api';
+  static const String simulationUrl = 'http://103.106.72.182:8040/api';
+  static const String gameUrl = 'http://103.106.72.182:8040/api/minigames';
 }
